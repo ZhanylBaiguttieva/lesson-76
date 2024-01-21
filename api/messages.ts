43653f2b103c3c1260ee5eee7148810ee5cc8088
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-21 22:35:28 +0600

    Added test for empty body message

--- api/messages.ts
+++ api/messages.ts
@@ -10,15 +10,21 @@ messagesRouter.get('/', async (req, res)=>{
     res.send(products);
 });
 
-messagesRouter.post('/', async(req, res)=>{
+messagesRouter.post('/', async(req, res, next)=>{
+    try {
+        const chat: Chat = {
+            message: req.body.message,
+            author: req.body.author,
+        };
 
-    const chat: Chat = {
-        message: req.body.message,
-        author: req.body.author,
-    };
-
-    const newChat = await fileDb.addItem(chat);
-    res.send(newChat);
+        if(!chat.message || !chat.author) {
+            return res.status(400).send({error: "Author or message are empty"})
+        }
+        const newChat = await fileDb.addItem(chat);
+        res.send(newChat);
+    } catch (e) {
+        next(e);
+    }
 });
 
 export default messagesRouter;
