6ab6301db76405e6da29278fa21603523048c7b6
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-21 23:51:10 +0600

    Added in request postChat: body in json

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -2,14 +2,14 @@ import React, {useState} from 'react';
 import {Chat} from '../../../types';
 import { Grid, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+import {request} from '../../../APIrequest.ts';
 
-interface Props {
-  onSubmit: (chat: Chat) => void;
-}
-const Form: React.FC<Props> = ({onSubmit}) => {
+const Form = () => {
   const [state, setState] = useState<Chat>({
+    id: '',
     message: '',
     author: '',
+    datetime: ''
   });
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,14 +20,35 @@ const Form: React.FC<Props> = ({onSubmit}) => {
     }));
   };
 
-  const submitFormHandler = (e: React.FormEvent) => {
+
+  const postChat = async (e: React.FormEvent) => {
     e.preventDefault();
-    onSubmit(state);
+    const url = 'http://localhost:8000/messages';
+
+    try {
+        const jsonData = {
+            message: state.message,
+            author: state.author,
+        };
+      await request<Chat>(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonData),
+      });
+    } catch (error) {
+      console.error('Error posting chat:', error);
+    }
+    setState((prev) => ({
+      ...prev,
+      message: '',
+      author: '',
+    }));
   };
 
-
   return (
-    <form onSubmit={submitFormHandler}>
+    <form onSubmit={postChat}>
       <Grid container direction="column" spacing={2}>
         <Grid item xs>
           <TextField
