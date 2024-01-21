0aebc64304f584c849f96bc3b60d5c515b5650a1
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-22 01:03:47 +0600

    Added  function creating id and datetime for chat

--- api/messages.ts
+++ api/messages.ts
@@ -4,16 +4,34 @@ import fileDb from "./fileDb";
 import {Chat} from "./types";
 
 const messagesRouter = Router();
-messagesRouter.get('/', async (req, res)=>{
-    const chats = await fileDb.getItems();
-    res.send(chats);
-});
+messagesRouter.get('/', async (req, res, next)=>{
+    try {
+        const queryDate = req.query.datetime as string;
+        const chats = await fileDb.getItems();
+        const filteredChats = chats.filter(currentChat => {
+            const cur = currentChat.datetime > queryDate;
+            console.log(cur);
+            return cur;
+        });
 
+        res.send(filteredChats);
+
+    }   catch (e) {
+        next(e);
+    }
+});
 messagesRouter.post('/', async(req, res, next)=>{
     try {
+
+        const now = new Date;
+        const datetime = now.toISOString();
+
+        const id = crypto.randomUUID();
         const chat: Chat = {
+            id: id,
             message: req.body.message,
             author: req.body.author,
+            datetime: datetime
         };
 
         if(!chat.message || !chat.author) {
